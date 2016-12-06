f8920113d756b1f47bb93a2612e29e6d9ca7904e
Mohith Damarapati <mohithdamarapati@gmail.com>
2016-12-06 10:39:56 +0530

    First Commit

---
+++ sudoku.py
@@ -0,0 +1,11 @@
+import pylab as pl 
+import numpy as np
+import cv2
+
+#Read Original Image Through WebCam
+sudoku_original = cv2.imread('sudoku_1.png')
+#Display Image On To Screen
+cv2.imshow('Sudoku-1',sudoku_original)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+

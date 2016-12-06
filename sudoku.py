efe40ba8c6e4ee8a679617aad1d5c005d0cb3a18
Mohith Damarapati <mohithdamarapati@gmail.com>
2016-12-06 10:56:16 +0530

    Second Commit

--- sudoku.py
+++ sudoku.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 #Read Original Image Through WebCam
-sudoku_original = cv2.imread('sudoku_1.png')
+sudoku_original = cv2.imread('sudoku-2.png')
 #Display Image On To Screen
 cv2.imshow('Sudoku-1',sudoku_original)
 cv2.waitKey(0)
